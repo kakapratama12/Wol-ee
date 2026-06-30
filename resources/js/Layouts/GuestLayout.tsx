36638cc3ad8e45d5a4de5f6dce2f6d52305c89f9
Ubuntu <ubuntu@localhost.localdomain>
2026-06-30 18:11:59 +0800

    feat: login page uses same brand logo as sidebar

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,4 +1,4 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+
 import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
 
@@ -7,7 +7,7 @@ export default function Guest({ children }: PropsWithChildren) {
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src="/logo.png" alt="Wol-ee" className="h-16 w-auto" />
                 </Link>
             </div>
 
